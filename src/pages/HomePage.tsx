eb9b96cbb980c044a81690ed2538890e24c79112
ROG <luan.roger.2003@gmail.com>
2024-01-17 18:29:28 -0300

    Initial commit

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <>
+      <div className="flex h-screen justify-center items-center">
+        <h1 className="text-4xl font-bold">Home Page</h1>
+      </div>
+    </>
+  );
+}
