4a79c911e61838ea58376789493edc30775f6f39
ROG <luan.roger.2003@gmail.com>
2024-01-18 18:16:48 -0300

    fix: Y overflow on text editor

--- src/components/RichTextEditor.tsx
+++ src/components/RichTextEditor.tsx
@@ -1,22 +1,19 @@
 import { ScrollArea } from "../components/ui/scroll-area";
-import {
-    Editor,
-  EditorContent,
-} from "@tiptap/react";
+import { Editor, EditorContent } from "@tiptap/react";
 import React from "react";
 
 interface EditorProps {
-    editor: Editor
+  editor: Editor;
 }
 
 export default function RichTextEditor({ editor }: EditorProps) {
   return (
-    <ScrollArea className="max-w-full h-full rounded-md border border-input bg-background flex-1
+    <div
+      className="w-full h-full scrollbar rounded-md overflow-y-scroll border border-input bg-background
     px-3 py-2 text-sm ring-offset-background focus-visible:outline-none focus-visible:ring-2
-     focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50">
-      <div>
-        <EditorContent editor={editor} content="Digite aqui..."/>
-      </div>
-    </ScrollArea>
+     focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+    >
+      <EditorContent editor={editor} content="Digite aqui..." />
+    </div>
   );
 }
